fa0ec936683b9b63106d14a9380b041d3b536350
Stefan <stefan@terminal21.de>
2012-05-29 01:42:56 +0200

    adding icecast xml parser

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ RRDTool_Icecast_Stats
 =====================
 
 Icecast current listener stats using RRDTool
+
+Perl CPAN-Modules
+-----------------
+
+You have to install the following Perl modules using our package manager or cpan
+
+- LWP::Simple
+- HTML::Parser
+- RRDTool::OO ;
+
+Creating the RR-Database
+------------------------
+
+> rrdtool create stream.rrd --step=60 \
+>   DS:listener:GAUGE:100:0:999       \
+>   RRA:LAST:0.5:1:20160              \
+>   RRA:AVERAGE:0.9:60:1488           \
+>   RRA:AVERAGE:0.99:1440:775         \
+>   RRA:MAX:0.9:60:1488               \
+>   RRA:MAX:0.99:1440:775
+
+Configuring the script
+----------------------
+
+In read_streamdata.pl customize the settings for
+
+- STREAM_SERVER_URL
+- STREAM_TITLE
+- RRD_FILE
---
+++ read_streamdata.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use constant STREAM_SERVER_URL => "http://streaming.fueralle.org:8000" ;
+use constant STREAM_TITLE => "Radio Corax MP3 Stream" ;
+use constant RRD_FILE => "./stream.rrd" ;
+use LWP::Simple ;
+use HTML::Parser ;
+use RRDTool::OO ;
+
+$listener = "U" ;
+
+$station_dataspace = 0 ;
+$listener_dataspace = 0 ;
+
+$counter = 0 ;
+
+$icecast_xml = get(STREAM_SERVER_URL) ;
+#die "Couldn't reach stream server" unless defined $icecast_xml ; # Unknown -> RRDTool ergaenzen
+
+$parser = HTML::Parser->new(api_version => 3, text_h => [ 'text_trigger', "text"]) ;
+$parser->parse($icecast_xml) ;
+
+sub text_trigger {
+
+  $counter++ ;
+
+  return unless ( $_[0] =~ /\S+/ ) ;  # Don't check empty strings
+
+  if ( $_[0]  eq STREAM_TITLE ) { # Icecast serves multiple streams. Is this our radio station?
+    $station_dataspace = 1 ;
+    return ;
+  }
+
+  if ( ($_[0] eq "Current Listeners:") &&  ($station_dataspace) ) { # Current listeners is what we are looking for
+    $listener_dataspace = $counter ;
+    return ;
+  }
+
+  if ( ($_[0] =~ /\d+/) && ($listener_dataspace) && ($counter-$listener_dataspace < 3)) { # The digits less then three text-events later represents listeners
+    $listener = $_[0] ;
+
+    $station_dataspace = 0 ;  # We have done, so we are out of scope
+    $listener_dataspace = 0 ;
+  }
+}
+
+$rrd = RRDTool::OO->new(file => RRD_FILE) ;
+die "Couldn't open RR-Database\n" unless defined $rrd ;
+
+$rrd->update($listener) ;
+
+#print "$listener\n" ;
+
